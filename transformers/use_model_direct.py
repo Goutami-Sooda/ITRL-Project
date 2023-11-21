e8a2d8cb92caed97a1ccac154df05c8a0c125f80
manvithlb <manvithlb@gmail.com>
2023-11-22 02:01:35 +0530

    Removed duplicated files

--- transformers/use_model_direct.py
+++
@@ -1,5 +0,0 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-tokenizer = AutoTokenizer.from_pretrained("AshArya/ITRLTrained")
-model = AutoModelForSeq2SeqLM.from_pretrained("AshArya/ITRLTrained")
--- transformers/use_model_pipeline.py
+++
@@ -1,6 +0,0 @@
-#to load the model using the pipeline method of the transformers library
-
-# Use a pipeline as a high-level helper
-from transformers import pipeline
-
-pipe = pipeline("text2text-generation", model="AshArya/ITRLTrained")
